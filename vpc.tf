1f5dfd6ef857fbca87318e1e50713c7affc51f3a
Eric Wright <eric.wright@turbonomic.com>
2020-10-10 10:42:43 -0400

    updating variables

--- vpc.tf
+++ vpc.tf
@@ -1,8 +1,10 @@
-resource "aws_vpc" "main" {
+resource "aws_vpc" "eks-lab-vpc" {
   cidr_block       = "10.0.0.0/16"
   instance_tenancy = "default"
 
   tags = {
     Name = "eks-lab"
+    Terraform = "true"
+    Turbonomic = "true"
   }
 }
