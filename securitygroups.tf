7758240c683416b6934ff5394f9cbb1b898e2303
Eric Wright <eric.wright@turbonomic.com>
2020-10-10 11:45:17 -0400

    updating variable

--- securitygroups.tf
+++ securitygroups.tf
@@ -12,21 +12,21 @@ resource "aws_security_group" "ssh-allowed" {
         from_port = 22
         to_port = 22
         protocol = "tcp"
-        cidr_blocks = [var.allowed_ip_network]
+        cidr_blocks = var.allowed_ip_network
     }
 
     ingress {
         from_port = 80
         to_port = 80
         protocol = "tcp"
-        cidr_blocks = [var.allowed_ip_network]
+        cidr_blocks = var.allowed_ip_network
     }
 
     ingress {
         from_port = 443
         to_port = 443
         protocol = "tcp"
-        cidr_blocks = [var.allowed_ip_network]
+        cidr_blocks = var.allowed_ip_network
     }
 
     tags = {
