19eaf3cd3d8c66771fe8a61d8feaf60c75f1e140
Eric Wright <eric.wright@turbonomic.com>
2020-10-10 12:20:05 -0400

    updating subnets

--- subnet.tf
+++ subnet.tf
@@ -17,7 +17,7 @@ resource "aws_subnet" "eks-lab-priv" {
     vpc_id = aws_vpc.eks-lab-vpc.id
     cidr_block = "10.0.10${count.index}.0/24"
     map_public_ip_on_launch = "false"
-    availability_zone = "${var.aws_region}a"
+    availability_zone = "${var.aws_region}${var.az_map[count.index]}"
 
     tags = {
     	Name = "eks-lab"
