cc6b31dee4cfa478dad0b94e8d600ae4750d8921
Eric Wright <eric.wright@turbonomic.com>
2020-10-10 10:14:54 -0400

    first commit

---
+++ vars.tf
@@ -0,0 +1,10 @@
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+variable "aws_region" {}
+variable "aws_vpc_id" {}
+variable "aws_key_name" {}
+variable "aws_key_fingerprint" {}
+variable "aws_certificate_arn" {}
+variable "aws_launch_subnet" {
+	default = "subnet-07d8310503d3d9704"
+}
