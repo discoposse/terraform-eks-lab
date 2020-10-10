ecb27101f8f93f4d827293782d19cf2518b8c601
Eric Wright <eric.wright@turbonomic.com>
2020-10-10 10:23:41 -0400

    update vars and provider

--- vars.tf
+++ vars.tf
@@ -5,6 +5,11 @@ variable "aws_vpc_id" {}
 variable "aws_key_name" {}
 variable "aws_key_fingerprint" {}
 variable "aws_certificate_arn" {}
-variable "aws_launch_subnet" {
-	default = "subnet-07d8310503d3d9704"
+variable "aws_launch_subnet" {}
+variable "terraform_workspace" {
+	default = "terraform-eks-lab"
+}
+
+variable "terraform_organization" {
+	default = "turbonomiclabs"
 }
